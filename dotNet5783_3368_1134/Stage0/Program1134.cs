c32476ee3318db86ca0fadc8b2ff8a6c8faf387a
Ori-Shoshana <orisho209@gmail.com>
2022-10-30 14:52:14 +0200

    Initial code + Welcome functions

---
+++ dotNet5783_3368_1134/Stage0/Program1134.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Targil0
+{
+    partial class Program
+    {
+        static void Main(String[] args)
+        {
+            Welcome1134();
+            Welcome3368();
+            Console.ReadKey();
+        }
+        static partial void Welcome3368();
+        private static void Welcome1134()
+        {
+            Console.Write("Enter your name: ");
+            string username = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", username);
+        }
+    }
+}
+
