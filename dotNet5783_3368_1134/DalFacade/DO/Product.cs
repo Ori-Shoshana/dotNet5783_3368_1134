d5fa7cd818276c6c436d3a56b7665b50eace7fe8
SapirBashan <sapirbashan1@gmail.com>
2022-11-06 15:08:10 +0200

    private id

--- dotNet5783_3368_1134/DalFacade/DO/Product.cs
+++ dotNet5783_3368_1134/DalFacade/DO/Product.cs
@@ -4,14 +4,14 @@ namespace DO;
 
 public struct Product
 {
-    public int IdPrivate { get; set; }
+    public int PrivateId { get; set; }
     public string ProdoctName { get; set; }
     public CategoryAttribute Category { get; set; }
     public double Price { get; set; }
     public int InStock { get; set; }
 
     public override string ToString() => $@"
-    private Id : {IdPrivate}
+    private Id : {PrivateId}
     Prodoct Name : {ProdoctName}
     Category : {Category}
     Price : {Price} 
