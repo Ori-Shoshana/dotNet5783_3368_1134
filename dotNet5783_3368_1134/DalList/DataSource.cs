a0f262783cda5527bac8afbb9694e2c546d5ce2d
Ori-Shoshana <orisho209@gmail.com>
2022-11-06 14:59:31 +0200

    changes

--- dotNet5783_3368_1134/DalList/DataSource.cs
+++ dotNet5783_3368_1134/DalList/DataSource.cs
@@ -14,35 +14,13 @@ internal static class DataSource
 
     private static void addOrder(int privateId, int orderId, int prodoctId, int price, int amount )
     {
-        Order newOrder = new Order();
-        newOrder.PrivateId = privateId;
-        newOrder.OrderId = orderId;
-        newOrder.ProdoctId = prodoctId;
-        newOrder.Price = price;
-        newOrder.Amount = amount;
-        //add to array!!!
+
     }
     private static void addOrderItem(int privateId, string customerName, string customerEmail, string customerAdrss, DateTime orderDate, DateTime shipDate, DateTime delivoryDate )
     { 
-        OrderItem newOrderItem = new OrderItem();
-        newOrderItem.PrivateId = privateId; 
-        newOrderItem.CustomerName = customerName;
-        newOrderItem.CustomerEmail= customerEmail;
-        newOrderItem.CustomerAdrss = customerAdrss; 
-        newOrderItem.OrderDate = orderDate;
-        newOrderItem.ShipDate = shipDate;
-        newOrderItem.DelivoryDate = delivoryDate;
-        //add to array1
     }
     private static void addProdoct(int privateId, string prodoctName, CategoryAttribute category, double price, int inStock)
-    {
-        Product newProdoct = new Product();
-        newProdoct.PrivateId = privateId;
-        newProdoct.ProdoctName = prodoctName;
-        newProdoct.Category = category;
-        newProdoct.Price = price;   
-        newProdoct.InStock = inStock;
-        //add to arrayS
+    { 
     }
     private static void s_Initialize()
     { 
