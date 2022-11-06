e709d6942e4fd0965eb4ba6eec98e68ca83ccea3
SapirBashan <sapirbashan1@gmail.com>
2022-11-06 19:02:44 +0200

    Datasource update

--- dotNet5783_3368_1134/DalList/DataSource.cs
+++ dotNet5783_3368_1134/DalList/DataSource.cs
@@ -10,36 +10,6 @@ internal static class DataSource
     internal static Order[] _order = new Order[100];
     internal static OrderItem[] _orderItem = new OrderItem[200];
     internal static Product[] _product = new Product[50];
-
-    private static void addOrder(int privateId, int orderId, int prodoctId, int price, int amount)
-    {
-        Order newOrder = new Order();
-        newOrder.PrivateId = privateId;
-        newOrder.OrderId = orderId;
-        newOrder.ProdoctId = prodoctId;
-        newOrder.Price = price;
-        newOrder.Amount = amount;
-    }
-    private static void addDalProduct(int privateId, string prodoctName, CategoryAttribute category, double price, int inStock)
-    {
-        Product newProdoct = new Product();
-        newProdoct.PrivateId = privateId;
-        newProdoct.ProdoctName = prodoctName;
-        newProdoct.Category = category;
-        newProdoct.Price = price;
-        newProdoct.InStock = inStock;
-    }
-    private static void addOrderItem(int privateId, string customerName, string customerEmail, string customerAdrss, DateTime orderDate, DateTime shipDate, DateTime delivoryDate)
-    {
-        OrderItem newOrderItem = new OrderItem();
-        newOrderItem.PrivateId = privateId;
-        newOrderItem.CustomerName = customerName;
-        newOrderItem.CustomerEmail = customerEmail;
-        newOrderItem.CustomerAdrss = customerAdrss;
-        newOrderItem.OrderDate = orderDate;
-        newOrderItem.ShipDate = shipDate;
-        newOrderItem.DelivoryDate = delivoryDate;
-    }
     internal static class config
     {
         internal static int OrderIndex = 0;
@@ -49,6 +19,25 @@ internal static class DataSource
     }
     private static void s_Initialize()
     {
+        s_order();
+        s_orderItem();
+        s_product();
     }
+
+    public static void s_order()
+    {
+
+    }
+
+    public static void s_orderItem()
+    {
+
+    }
+
+    public static void s_product()
+    {
+
+    }
+
 }
 
