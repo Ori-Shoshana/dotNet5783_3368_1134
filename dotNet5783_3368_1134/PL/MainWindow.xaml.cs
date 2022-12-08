005966f134635bec9e39e2891b2649ac346d75e9
SapirBashan <sapirbashan1@gmail.com>
2022-12-08 20:46:12 +0200

    update movieng butten

--- dotNet5783_3368_1134/PL/MainWindow.xaml.cs
+++ dotNet5783_3368_1134/PL/MainWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -32,6 +33,36 @@ namespace PL
         {
             new Product.ProductList().Show();
             Close();
+        }
+
+        Random rnd = new Random();
+
+        private void Button_MouseEnter(object sender, MouseEventArgs e)
+        {
+            //Work out where the button is going to move to.
+            double newLeft = rnd.Next(Convert.ToInt32(cnv.ActualWidth - btn.ActualWidth));
+            double newTop = rnd.Next(Convert.ToInt32(cnv.ActualHeight*10 - btn.ActualHeight));
+
+            //Create the animations for left and top
+            DoubleAnimation animLeft = new DoubleAnimation(Canvas.GetLeft(btn), newLeft, new Duration(TimeSpan.FromSeconds(1)));
+            DoubleAnimation animTop = new DoubleAnimation(Canvas.GetTop(btn), newTop, new Duration(TimeSpan.FromSeconds(1)));
+
+            //Set an easing function so the button will quickly move away, then slow down
+            //as it reaches its destination.
+            animLeft.EasingFunction = new CubicEase() { EasingMode = EasingMode.EaseOut };
+            animTop.EasingFunction = new CubicEase() { EasingMode = EasingMode.EaseOut };
+
+            //Start the animation.
+            btn.BeginAnimation(Canvas.LeftProperty, animLeft, HandoffBehavior.SnapshotAndReplace);
+            btn.BeginAnimation(Canvas.TopProperty, animTop, HandoffBehavior.SnapshotAndReplace);
+
+
+            if (btn.IsPressed)
+            {
+                new CatchMe.CatchMeIfYouCan().Show();
+                Close();
+            }
+            
         }
     }
 }
