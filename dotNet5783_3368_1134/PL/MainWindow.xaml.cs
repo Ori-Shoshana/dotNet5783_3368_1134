0514ca8343507ac687df7dbe4a8b564d89ad74df
SapirBashan <sapirbashan1@gmail.com>
2022-12-15 17:46:43 +0200

    update Main window xaml

--- dotNet5783_3368_1134/PL/MainWindow.xaml.cs
+++ dotNet5783_3368_1134/PL/MainWindow.xaml.cs
@@ -37,30 +37,30 @@ namespace PL
 
         Random rnd = new Random();
 
-        //private void Button_MouseEnter(object sender, MouseEventArgs e)
-        //{
-        //    //Work out where the button is going to move to.
-        //    double newLeft = rnd.Next(Convert.ToInt32(cnv.ActualWidth - btn.ActualWidth));
-        //    double newTop = rnd.Next(Convert.ToInt32(cnv.ActualHeight*10 - btn.ActualHeight));
+        private void Button_MouseEnter(object sender, MouseEventArgs e)
+        {
+            //Work out where the button is going to move to.
+            double newLeft = rnd.Next(Convert.ToInt32(cnv.ActualWidth - btn.ActualWidth));
+            double newTop = rnd.Next(Convert.ToInt32(cnv.ActualHeight * 10 - btn.ActualHeight));
 
-        //    //Create the animations for left and top
-        //    DoubleAnimation animLeft = new DoubleAnimation(Canvas.GetLeft(btn), newLeft, new Duration(TimeSpan.FromSeconds(1)));
-        //    DoubleAnimation animTop = new DoubleAnimation(Canvas.GetTop(btn), newTop, new Duration(TimeSpan.FromSeconds(1)));
+            //Create the animations for left and top
+            DoubleAnimation animLeft = new DoubleAnimation(Canvas.GetLeft(btn), newLeft, new Duration(TimeSpan.FromSeconds(1)));
+            DoubleAnimation animTop = new DoubleAnimation(Canvas.GetTop(btn), newTop, new Duration(TimeSpan.FromSeconds(1)));
 
-        //    //Set an easing function so the button will quickly move away, then slow down
-        //    //as it reaches its destination.
-        //    animLeft.EasingFunction = new CubicEase() { EasingMode = EasingMode.EaseOut };
-        //    animTop.EasingFunction = new CubicEase() { EasingMode = EasingMode.EaseOut };
+            //Set an easing function so the button will quickly move away, then slow down
+            //as it reaches its destination.
+            animLeft.EasingFunction = new CubicEase() { EasingMode = EasingMode.EaseOut };
+            animTop.EasingFunction = new CubicEase() { EasingMode = EasingMode.EaseOut };
 
-        //    //Start the animation.
-        //    btn.BeginAnimation(Canvas.LeftProperty, animLeft, HandoffBehavior.SnapshotAndReplace);
-        //    btn.BeginAnimation(Canvas.TopProperty, animTop, HandoffBehavior.SnapshotAndReplace);          
-        //}
+            //Start the animation.
+            btn.BeginAnimation(Canvas.LeftProperty, animLeft, HandoffBehavior.SnapshotAndReplace);
+            btn.BeginAnimation(Canvas.TopProperty, animTop, HandoffBehavior.SnapshotAndReplace);
+        }
 
-        //private void btn_MouseDoubleClick(object sender, MouseButtonEventArgs e)
-        //{
-        //    new CatchMe.CatchMeIfYouCan().Show();  
-        //}
+        private void btn_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            new CatchMe.CatchMeIfYouCan().Show();
+        }
 
         private void btn_Click(object sender, RoutedEventArgs e)
         {
