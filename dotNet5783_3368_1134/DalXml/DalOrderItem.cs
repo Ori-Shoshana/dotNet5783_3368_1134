aabca7184077d3d0dcec2d0664a45c9301bdb7d1
SapirBashan <sapirbashan1@gmail.com>
2023-01-11 18:13:06 +0200

    update xml Tools

--- dotNet5783_3368_1134/DalXml/DalOrderItem.cs
+++ dotNet5783_3368_1134/DalXml/DalOrderItem.cs
@@ -1,12 +1,49 @@
-﻿using System;
+﻿using DalApi;
+using DO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
-namespace Dal
+namespace Dal;
+
+internal class DalOrderItem : IOrderItem
 {
-    internal class DalOrderItem
+    public int Add(OrderItem entity)
     {
+
+        throw new NotImplementedException();
+    }
+
+    public void Delete(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<OrderItem?> GetAll(Func<OrderItem?, bool>? func = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public OrderItem GetByDelegate(Func<OrderItem?, bool>? func)
+    {
+        throw new NotImplementedException();
+    }
+
+    public OrderItem GetById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public int ListLength()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(OrderItem entity)
+    {
+        throw new NotImplementedException();
     }
 }
