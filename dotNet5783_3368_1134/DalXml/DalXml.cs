c7010a3e5bfa1e4183ce8dc079a0fd75605f54dd
SapirBashan <sapirbashan1@gmail.com>
2023-01-11 20:44:18 +0200

    update little change

--- dotNet5783_3368_1134/DalXml/DalXml.cs
+++ dotNet5783_3368_1134/DalXml/DalXml.cs
@@ -12,7 +12,7 @@ sealed internal class DalXml : IDal
 {
     private DalXml() { } // constructor stage 6
     public static IDal Instance { get; } = new DalXml(); // stage 6
-    public IOrder order { get; } = new DalOrder();
+    public IOrder Order { get; } = new DalOrder();
     public IProduct Product { get; } = new DalProduct();
     public IOrderItem OrderItem { get; } = new DalOrderItem();
 }
